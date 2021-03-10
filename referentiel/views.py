d136dbc3c073b7cf3f1df35f249671e704a45e92
b-henriques <75887092+b-henriques@users.noreply.github.com>
2021-03-10 15:57:08 +0100

    correction de qlqs oublis

--- referentiel/views.py
+++ referentiel/views.py
@@ -4,4 +4,12 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("page ref.")
+
+
+def detail(request, article_id):
+    return HttpResponse("You're looking at question %s." % article_id)
+
+def results(request, article_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % article_id)
