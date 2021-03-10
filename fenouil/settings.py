0baa670a7a6a5a6844239db5430815f8b7937118
b-henriques <75887092+b-henriques@users.noreply.github.com>
2021-03-10 15:20:27 +0100

    page_admin_django

--- fenouil/settings.py
+++ fenouil/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'referentiel.apps.ReferentielConfig',
+    'publicite.apps.PubliciteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- referentiel/models.py
+++ referentiel/models.py
@@ -7,27 +7,43 @@ class Article(models.Model):
     designation = models.CharField(max_length=200)
     prix = models.DecimalField(max_digits=6, decimal_places=2)
 
+    def __str__(self) -> str:
+        return self.designation
 
-class catprofessionnelle(models.Model):
+
+class Catprofessionnelle(models.Model):
     nom = models.CharField(max_length=100)
 
+    def __str__(self) -> str:
+        return self.nom
 
-class catcommerciale(models.Model):
+
+class Catcommerciale(models.Model):
     nom = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.nom
 
 
 class Adresse(models.Model):
     numero = models.IntegerField()
     rue = models.CharField(max_length=100)
-    codepostal = models.IntegerField(max_length=5)
+    codepostal = models.DecimalField(max_digits=5, decimal_places=0)
     ville = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return str(self.numero)+self.rue+"\n"+str(self.codepostal)+self.ville
 
 
 class Individu(models.Model):
     nom = models.CharField(max_length=50)
     prenom = models.CharField(max_length=50)
     datenaissance = models.DateField()
-    ntel = models.IntegerField(max_length=10)
-    mail = models.EmailField()
+    ntel = models.DecimalField(max_digits=10, decimal_places=0)
+    mail = models.EmailField(null=True)
     adresse = models.ForeignKey(Adresse, on_delete=models.CASCADE)
-    adresse = models.ForeignKey(catprofessionnelle, on_delete=models.CASCADE)
+    catprofessionnelle = models.ForeignKey(Catprofessionnelle, on_delete=models.CASCADE)
+    catcommerciale = models.ForeignKey(Catcommerciale, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return "(id = "+str(self.id)+", nom= "+self.nom+", prenom= "+self.prenom
