21791108ce1b5a5ed364efc270a9433a6ad88282
b-henriques <75887092+b-henriques@users.noreply.github.com>
2021-03-10 16:25:05 +0100

    page creation article

--- referentiel/views.py
+++ referentiel/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-
+#page admin ref
 def index(request):
     return HttpResponse("page ref.")
 
-
+#page detail article
 def detail(request, article_id):
-    return HttpResponse("You're looking at question %s." % article_id)
+    return HttpResponse("informations aricle %s." % article_id)
 
-def results(request, article_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % article_id)
+#page creation article
+def creation(request):
+    return render(request, "referentiel/createArticle.html")
+
+#page creation individu
+
