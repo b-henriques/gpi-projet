fbd04e5f783608e594b6d942e9ea0b90c47cecad
Alexandre <20183634@etud.univ-evry.fr>
2021-03-12 19:00:06 +0100

    afficheArticle.html
    Ajout de la page qui affiche la table "Articles"

--- referentiel/urls.py
+++ referentiel/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'referentiel'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('affiche_article', views.affiche_article, name='affiche_article')
 ]
--- referentiel/views.py
+++ referentiel/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'referentiel/createArticle.html', {})
+
+def affiche_article(request):
+    return render(request, 'referentiel/afficheArticle.html', {})
