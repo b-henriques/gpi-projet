6218aebe026ef7e284e3856861d5c33cff9f12a1
b-henriques <75887092+b-henriques@users.noreply.github.com>
2021-03-13 21:12:30 +0100

    logIn page + AdministrateurReferentiel Group

--- utilisateurs/admin.py
+++ utilisateurs/admin.py
@@ -1,3 +1,5 @@
+from utilisateurs.models import Utilisateur
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Utilisateur)
--- utilisateurs/views.py
+++ utilisateurs/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
-from django.http.response import HttpResponse
-from django.shortcuts import render
 
-# Create your views here.
+
 def index(request):
-    return HttpResponse("<h1> Utilisateurs: Connexion </h1>")
+    message = False
+    if 'logIn' in request.POST:
+        identifiant = request.POST['identifiant']
+        motdepass = request.POST['motdepass']
+        utilisateur = authenticate(
+            request, username=identifiant, password=motdepass)
+        if utilisateur is not None:
+            login(request, utilisateur)
+            # succes = redirect a une page en fonction du groupe
+            return redirect('referentiel/')
+        else:
+            message = True
+    contexte = {'messageErreur': message}
+    return render(request, 'utilisateurs/logIn.html', contexte)
