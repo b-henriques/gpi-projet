074070b67db3014493369f82e9061a051714aa98
b-henriques <75887092+b-henriques@users.noreply.github.com>
2021-03-27 19:19:26 +0100

    Modification Article + Modification Individu

--- referentiel/urls.py
+++ referentiel/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('articles', views.tableArticle, name='affichageReferentielArticles'),
     path('individus', views.tableIndividu, name='affichageReferentielIndividus'),
     path('delarticle/<str:pk>', views.delArticle, name='supressionArticle'),
-    path('delindividu/<str:pk>', views.delIndividu, name='supressionIndividu')
+    path('delindividu/<str:pk>', views.delIndividu, name='supressionIndividu'),
+    path('modifArticle/<str:pk>', views.modifierArticle, name='modifArticle'),
+    path('modifIndividu/<str:pk>', views.modifierIndividu, name='modifIndividu')
 ]
--- referentiel/views.py
+++ referentiel/views.py
@@ -3,13 +3,17 @@ from .models import Adresse, Catprofessionnelle, Individu, Article
 from .forms import ArticleForm, IndividuForm
 from django.http.response import HttpResponse
 from django.contrib.auth.decorators import permission_required
+from django.urls import reverse
 
-#TODO: possibilite de creer cat pro en meme temps que Ind
-#TODO: modification Art ou Ind
+# TODO: possibilite de creer cat pro en meme temps que Ind
+# TODO: modification Art ou Ind
 
-#@permission_required('perm_administrerRef')
+# @permission_required('perm_administrerRef')
+
+
 def index(request):
-    return HttpResponse("<h1> Referentiel </h1>")
+    return render(request, 'referentiel/referentiel_view.html')
+
 
 def creationReferentiel(request):
     # affiche le cde html de form
@@ -25,7 +29,7 @@ def creationReferentiel(request):
     if form2.is_valid():
         print(True)
         # affiche les donnees du formulaire
-        #print(form2.cleaned_data)
+        # print(form2.cleaned_data)
 
         # creer adress
         adresse = Adresse(
@@ -36,14 +40,14 @@ def creationReferentiel(request):
         )
         adresse.save()
         individu = Individu(
-            nom= form2.cleaned_data['nom'],
-            prenom = form2.cleaned_data['prenom'],
-            datenaissance = form2.cleaned_data['datenaissance'],
-            ntel = form2.cleaned_data['ntel'],
-            mail = form2.cleaned_data['mail'],
-            adresse = adresse,
-            catcommerciale = form2.cleaned_data['catcommerciale'],
-            catprofessionnelle = form2.cleaned_data['catprofessionnelle'],
+            nom=form2.cleaned_data['nom'],
+            prenom=form2.cleaned_data['prenom'],
+            datenaissance=form2.cleaned_data['datenaissance'],
+            ntel=form2.cleaned_data['ntel'],
+            mail=form2.cleaned_data['mail'],
+            adresse=adresse,
+            catcommerciale=form2.cleaned_data['catcommerciale'],
+            catprofessionnelle=form2.cleaned_data['catprofessionnelle'],
         )
         individu.save()
 
@@ -52,12 +56,14 @@ def creationReferentiel(request):
 
 
 def tableArticle(request):
-    contexte = { 'articles' : Article.objects.all()}
+    contexte = {'articles': Article.objects.all()}
     return render(request, 'referentiel/afficheArticle.html', contexte)
 
+
 def tableIndividu(request):
-    contexte = { 'individus' : Individu.objects.all()}
+    contexte = {'individus': Individu.objects.all()}
     return render(request, 'referentiel/afficheIndividu.html', contexte)
+
 
 def delArticle(request, pk):
     article = get_object_or_404(Article, pk=pk)
@@ -67,8 +73,9 @@ def delArticle(request, pk):
         else:
             article.delete()
             return redirect('/referentiel/articles')
-    contexte = { 'article' : article}
+    contexte = {'article': article}
     return render(request, 'referentiel/deletearticle.html', contexte)
+
 
 def delIndividu(request, pk):
     individu = get_object_or_404(Individu, pk=pk)
@@ -78,5 +85,51 @@ def delIndividu(request, pk):
         else:
             individu.delete()
             return redirect('/referentiel/individus')
-    contexte = { 'individu' : individu}
+    contexte = {'individu': individu}
     return render(request, 'referentiel/deleteindividu.html', contexte)
+
+
+def modifierArticle(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    form = ArticleForm(request.POST or None, instance=article)
+    if form.is_valid():
+        form.save()
+        return redirect(reverse("referentiel:affichageReferentielArticles"))
+    contexte = {'form': form}
+    return render(request, 'referentiel/modifArticle.html', contexte)
+
+
+def modifierIndividu(request, pk):
+    individu = get_object_or_404(Individu, pk=pk)
+    form = IndividuForm(request.POST or None, initial={
+        'nom' : individu.nom,
+        'prenom' : individu.prenom,
+        'datenaissance' : individu.datenaissance,
+        'ntel' : individu.ntel,
+        'mail' : individu.mail,
+        'catcommerciale' : individu.catcommerciale,
+        'catprofessionnelle' : individu.catprofessionnelle,
+        'numero' : individu.adresse.numero,
+        'rue' : individu.adresse.rue,
+        'codepostal' : individu.adresse.codepostal,
+        'ville' : individu.adresse.ville 
+    })
+    if form.is_valid():
+        adresse = individu.adresse
+        adresse.numero=form.cleaned_data['numero']
+        adresse.rue=form.cleaned_data['rue']
+        adresse.codepostal=form.cleaned_data['codepostal']
+        adresse.ville=form.cleaned_data['ville']
+        adresse.save()
+        individu.nom=form.cleaned_data['nom']
+        individu.prenom=form.cleaned_data['prenom']
+        individu.datenaissance=form.cleaned_data['datenaissance']
+        individu.ntel=form.cleaned_data['ntel']
+        individu.mail=form.cleaned_data['mail']
+        individu.adresse=adresse
+        individu.catcommerciale=form.cleaned_data['catcommerciale']
+        individu.catprofessionnelle=form.cleaned_data['catprofessionnelle']
+        individu.save()
+        return redirect(reverse("referentiel:affichageReferentielIndividus"))
+    contexte = {'form': form, 'id':pk}
+    return render(request, 'referentiel/modifIndividu.html', contexte)
